dbc4714a167c650b95a46db2addd0e324b76c580
KishanPatel1410 <kishanpatel141006@gmail.com>
2025-06-25 12:18:55 +0530

    simple if

---
+++ simple if.c
@@ -0,0 +1,16 @@
+//simple if program
+
+#include<stdio.h>
+int main()
+{
+int a;
+printf("\neneter the value of a :");
+scanf("%d",&a);//2
+
+if(a>0)//2>0 true
+{
+printf("\neneterd number is positive");
+}
+
+printf("\nthank you");
+}
