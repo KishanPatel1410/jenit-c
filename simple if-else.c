2bcdf6513e1487a95b38ae7c25f871108489d99c
KishanPatel1410 <kishanpatel141006@gmail.com>
2025-06-25 12:19:15 +0530

    simple if-else

---
+++ simple if-else.c
@@ -0,0 +1,18 @@
+//if-else
+#include<stdio.h>
+int main()
+{
+
+	int a;
+	printf("\neneter the number:");
+	scanf("%d",&a);
+	if(a%2==0)
+	{
+		printf("\neneted number is even");
+	}
+	else
+	{
+		printf("\neneted number is odd");
+		
+	}
+}
